05955e2652e87774766bb11a771b27fdcb21f549
Amit <amit.vrma11111@gmail.com>
2018-05-24 18:43:24 +0530

    conatct form

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home_page(request):
-	return render(request,'home_page.html',{})
+from .forms import ContactForm
 
+def home_page(request):
+	context ={
+	"title":"Hello World"
+	}
+	return render(request,'home_page.html',context)
+
+def about_page(request):
+	context = {
+	"title":"About"
+	}
+	return render(request,'home_page.html',context)
+
+def contact_page(request):
+	contact_form = ContactForm(request.POST or None)
+	context = {
+	"title":"Contact",
+	"form":contact_form
+	}
+	if contact_form.is_valid():
+		print(contact_form.cleaned_data)
+	#if request.method=="POST":
+	#	print(request.POST)	
+	return render(request,'contact/view.html',context)

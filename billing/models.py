7a52be4b2ffc625b85e9b646218b8e33589ad496
Amit <amit.vrma11111@gmail.com>
2018-07-13 22:51:03 +0530

    billing profile model

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import pre_save,post_save
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+# abc@gmail.com -> 10000 billing profiles
+# user abc@gmail.com -> 1 billing profile
+
+class BillingProfile(models.Model):
+	user = models.OneToOneField(User, blank=True,null=True)
+	email = models.EmailField()
+	active = models.BooleanField(default=True)
+	update = models.DateTimeField(auto_now=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
+	#customer_id in stripe/BrainTree
+
+	def __str__(self):
+		return self.email
+
+# def billing_profile_created_receiver(sender,instance,created, *args,**kwargs):
+# 	if created:
+# 		print("Actual API call to Stripe/Braintree")
+# 		instance.customer_id = newID
+# 		instance.save()
+
+
+def user_created_receiver(sender,instance, created ,*args, **kwargs):
+	if created and instance.email:
+		BillingProfile.objects.get_or_create(user=instance, email = instance.email)
+
+
+post_save.connect(user_created_receiver, sender = User)

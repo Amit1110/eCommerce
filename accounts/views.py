4da56d91090627b37f5d27d41b15138d50380727
Amit <amit.vrma11111@gmail.com>
2018-08-19 23:29:02 +0530

    Login and Register Views Updated

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.views.generic import CreateView, FormView
 from django.contrib.auth import authenticate,login,get_user_model
 from django.utils.http import is_safe_url
 
@@ -28,20 +29,20 @@ def guest_register_view(request):
 		
 	return redirect("/register/")
 
+class LoginView(FormView):
+	form_class = LoginForm
+	success_url = '/'
+	template_name = 'accounts/login.html'
 
-def login_page(request):
-	form = LoginForm(request.POST or None)
-	context = {
-	"form":form
-	}
-	next_ = request.GET.get('next')
-	next_post = request.POST.get('next')
-	redirect_path = next_ or next_post or None
-	if form.is_valid():
+	def form_valid(self,form):
+		request = self.request
+		next_ = request.GET.get('next')
+		next_post = request.POST.get('next')
+		redirect_path = next_ or next_post or None
 		print(form.cleaned_data)
-		username = form.cleaned_data.get("username")
+		email = form.cleaned_data.get("email")
 		password = form.cleaned_data.get("password")
-		user = authenticate(request,username=username,password=password)
+		user = authenticate(request,email=email,password=password)
 		if user is not None:
 			login(request,user)
 			try:
@@ -52,23 +53,54 @@ def login_page(request):
 				return redirect(redirect_path)
 			else:
 				return redirect('/')
-		else:
-			#Return an 'invalid login' error message 
-			print("Error")
+		return super(LoginView, self).form_invalid(form)
 
-		
-	return render(request,'accounts/login.html',context)
 
 User = get_user_model()
 
 
-def register_page(request):
-	form = RegisterForm(request.POST or None)
-	context = {
-	"form":form
-	}
-	if form.is_valid():
-		form.save()
+class RegisterView(CreateView):
+	form_class = RegisterForm
+	template_name = 'accounts/register.html'
+	success_url = '/login/'
 
-	
-	return render(request,'accounts/register.html',context)
+# def register_page(request):
+# 	form = RegisterForm(request.POST or None)
+# 	context = {
+# 	"form":form
+# 	}
+# 	if form.is_valid():
+# 		form.save()
+# 	return render(request,'accounts/register.html',context)
+
+
+
+#def login_page(request):
+# 	form = LoginForm(request.POST or None)
+# 	context = {
+# 	"form":form
+# 	}
+# 	next_ = request.GET.get('next')
+# 	next_post = request.POST.get('next')
+# 	redirect_path = next_ or next_post or None
+# 	if form.is_valid():
+# 		print(form.cleaned_data)
+# 		username = form.cleaned_data.get("username")
+# 		password = form.cleaned_data.get("password")
+# 		user = authenticate(request,username=username,password=password)
+# 		if user is not None:
+# 			login(request,user)
+# 			try:
+# 				del request.session["guest_email_id"]
+# 			except:
+# 				pass
+# 			if is_safe_url(redirect_path, request.get_host()):
+# 				return redirect(redirect_path)
+# 			else:
+# 				return redirect('/')
+# 		else:
+# 			#Return an 'invalid login' error message 
+# 			print("Error")
+
+		
+# 	return render(request,'accounts/login.html',context)
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -8,7 +8,7 @@ from django.contrib.auth.views import LogoutView
 # from products.views import (ProductListView, productlistview,ProductDetailView, productdetailview,
 #     ProductFeaturedListView,ProductFeaturedDetailView,ProductDetailSlugView)
 
-from accounts.views import login_page,register_page,guest_register_view
+from accounts.views import LoginView,RegisterView,guest_register_view
 from .views import home_page,contact_page,about_page
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
 from carts.views import cart_detail_api_view
@@ -19,7 +19,7 @@ urlpatterns = [
     url(r'^$', home_page, name='home'),
     url(r'^about/$', about_page,name='about'),
     url(r'^contact/$', contact_page,name='contact'),
-    url(r'^login/$', login_page,name='login'),
+    url(r'^login/$', LoginView.as_view(),name='login'),
     url(r'^checkout/address/create/$',checkout_address_create_view,name='checkout_address_create'),
     url(r'^checkout/address/reuse/$',checkout_address_reuse_view,name='checkout_address_reuse'),
     url(r'^register/guest/$', guest_register_view,name='guest_register'),
@@ -27,7 +27,7 @@ urlpatterns = [
     url(r'^products/', include("products.urls",namespace = 'products')),
     url(r'^api/cart/$', cart_detail_api_view,name='api-cart'),
     url(r'^cart/', include("carts.urls",namespace = 'cart')),
-    url(r'^register/$', register_page,name='register'),
+    url(r'^register/$', RegisterView.as_view(),name='register'),
     url(r'^bootstrap/$', TemplateView.as_view(template_name = 'bootstrap/example.html')),
     url(r'^search/', include("search.urls",namespace = 'search')),   
     # url(r'^product-fbv/$', productlistview),

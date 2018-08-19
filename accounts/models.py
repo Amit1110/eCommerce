750476267e78825cf17a6d4a7e0b76f82ea9605a
Amit <amit.vrma11111@gmail.com>
2018-08-19 15:50:50 +0530

    new User Model added

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,39 @@
 from django.db import models
 from django.contrib.auth.models import (
-	AbstractBaseUser
+	AbstractBaseUser, BaseUserManager
 )
+
+class UserManager(BaseUserManager):
+	def create_user(self,email,password=None, is_active = True, is_staff=False,is_admin=False):
+		if not email:
+			raise ValueError("Users must have an email address")
+		if not password:
+			raise ValueError("Users must have a password")
+		user_obj =  self.model(
+			email = self.normalize_email(email)
+		)
+		user_obj.staff = is_staff
+		user_obj.active = is_active
+		user_obj.admin = is_admin
+		user_obj.set_password(password) # change user password also
+		user_obj.save(using=self._db)
+		return user_obj
+
+	def create_staffuser(self,email,password=None):
+		user = self.create_user(
+			email,
+			password = password,
+			is_staff=True
+		)
+		return user
+	def create_superuser(self,email,password=None):
+		user = self.create_user(
+			email,
+			password = password,
+			is_staff=True,
+			is_admin = True
+		)
+		return user
 
 class User(AbstractBaseUser):
 	email = models.EmailField(max_length = 255,unique=True)
@@ -12,7 +44,9 @@ class User(AbstractBaseUser):
 	timestamp = models.DateTimeField(auto_now_add=True)
 
 	USERNAME_FIELD = 'email'
-	REQUIRED_FIELDS = ['']
+	REQUIRED_FIELDS = []
+
+	objects = UserManager()
 
 	def __Str__(self):
 		return self.email
@@ -23,15 +57,15 @@ class User(AbstractBaseUser):
 		return self.email
 
 	@property
-	def is_staff(Delf):
+	def is_staff(self):
 		return self.staff
 
 	@property
-	def is_admin(Delf):
+	def is_admin(self):
 		return self.admin
 
 	@property
-	def is_active(Delf):
+	def is_active(self):
 		return self.active
 
 

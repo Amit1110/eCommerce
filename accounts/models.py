65a6032ff0f16618268a5181152b861ef790e9f4
Amit <amit.vrma11111@gmail.com>
2018-08-19 14:45:23 +0530

    Absrtact User created

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import (
+	AbstractBaseUser
+)
+
+class User(AbstractBaseUser):
+	email = models.EmailField(max_length = 255,unique=True)
+	#full_name = models.CharField(max_length=255, blank = True, null=True)
+	active = models.BooleanField(default=True)#can login
+	staff = models.BooleanField(default=False)#staff user
+	admin = models.BooleanField(default=False)#superuser
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['']
+
+	def __Str__(self):
+		return self.email
+	def get_full_name(self):
+		return self.email
+
+	def get_short_name(self):
+		return self.email
+
+	@property
+	def is_staff(Delf):
+		return self.staff
+
+	@property
+	def is_admin(Delf):
+		return self.admin
+
+	@property
+	def is_active(Delf):
+		return self.active
+
+
 
 class GuestEmail(models.Model):
 	email = models.EmailField()

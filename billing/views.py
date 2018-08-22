409cd823f51f403bfe54519c4fd1fc2ec6423b11
Amit <amit.vrma11111@gmail.com>
2018-08-22 13:11:22 +0530

    payment method added

--- billing/views.py
+++ billing/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from django.http import JsonResponse,HttpResponse
+from django.utils.http import is_safe_url
+import stripe
 
+
+
+
+stripe.api_key = "sk_test_OBJ1L6pbEG137xczBIVLrp6n"
 STRIPE_PUB_KEY = "pk_test_ThzzgkBGhdMHqFaMi093UcFe"
 
 def payment_method_view(request):
-	if request.method=="POST":
+	# if request.method=="POST":
+	# 	print(request.POST)
+	next_url = None
+	next_ = request.GET.get('next')
+	if is_safe_url(next_, request.get_host()):
+		next_url = next_
+	return render(request,'billing/payment-method.html',{"publish_key":STRIPE_PUB_KEY, "next_url":next_url})
+
+
+def payment_method_createview(request):
+	if request.method=="POST" and request.is_ajax():
 		print(request.POST)
-	return render(request,'billing/payment-method.html',{"publish_key":STRIPE_PUB_KEY})
+		return JsonResponse({"message":"Success! Your card was added."})
+	return HttpResponse("error", status=401)
 
 

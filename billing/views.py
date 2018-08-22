f29e1d68661a778043e02ee9ff4ad6f50137b7d0
Amit <amit.vrma11111@gmail.com>
2018-08-22 11:28:01 +0530

    payment view added

--- billing/views.py
+++ billing/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+STRIPE_PUB_KEY = "pk_test_ThzzgkBGhdMHqFaMi093UcFe"
+
+def payment_method_view(request):
+	if request.method=="POST":
+		print(request.POST)
+	return render(request,'billing/payment-method.html',{"publish_key":STRIPE_PUB_KEY})
 
 

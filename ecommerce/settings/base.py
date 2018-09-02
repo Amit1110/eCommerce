aaa3f76fce3a0aa3c7381240d9bb1757093b486a
Amit <amit.vrma11111@gmail.com>
2018-09-02 19:45:19 +0530

    requirements added

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -160,3 +160,18 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn","media_root")
 pass - asdf1234
 all emails
 '''
+
+'''
+Let's encrypt ssl/tls certificate
+
+'''
+
+CORS_REPLACE_HTTPS_REFERER      = False
+HOST_SCHEME                     = "http://"
+SECURE_PROXY_SSL_HEADER         = None
+SECURE_SSL_REDIRECT             = False
+SESSION_COOKIE_SECURE           = False
+CSRF_COOKIE_SECURE              = False
+SECURE_HSTS_SECONDS             = None
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = False
+SECURE_FRAME_DENY               = False
